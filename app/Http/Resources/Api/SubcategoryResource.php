6fd48e0ad9c70cccdd84414e4ce126f0e473fb16
jtlaquec <78872016+jtlaquec@users.noreply.github.com>
2023-12-16 00:41:16 -0500

    API: URL para Subcategorías

--- app/Http/Resources/Api/SubcategoryResource.php
+++ app/Http/Resources/Api/SubcategoryResource.php
@@ -13,6 +13,7 @@ class SubcategoryResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'link' => url('api/store/subcategory/' . $this->id),
             'products' => ProductResource::collection($this->whenLoaded('products')),
         ];
     }
